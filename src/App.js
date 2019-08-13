e752128a5df12f5b4568ce8fd09c5bfc5453ba52
fmelp <melpignanof@gmail.com>
2019-08-13 18:01:27 -0400

    cleaned up ui

--- src/App.js
+++ src/App.js
@@ -2,20 +2,34 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CallPact from "./CallPact";
+import { Grid, Image } from "semantic-ui-react";
 
-
+import backgroundImage from './assets/background_logo_square.jpg';
 
 function App() {
 
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <h1 style={{color:"purple"}}>Welcome to the Kadena Testnet Faucet!</h1>
+  //       <CallPact/>
+  //     </header>
+  //     <div style={{position: "absolute", top: 10, left: 10}}>
+  //         <img src={require('./kadena.png')} />
+  //     </div>
+  //   </div>
+  // );
   return (
     <div className="App">
-      <header className="App-header">
-        <h1 style={{color:"purple"}}>Welcome to the Kadena Testnet Faucet!</h1>
-        <CallPact/>
-      </header>
-      <div style={{position: "absolute", top: 10, left: 10}}>
-            <img src={require('./kadena.png')} />
-          </div>
+      <Grid columns={2} verticalAlign='middle'>
+        <Grid.Column>
+          <Image src={backgroundImage} fluid={true} />
+        </Grid.Column>
+        <Grid.Column>
+          <CallPact />
+        </Grid.Column>
+      </Grid>
+
     </div>
   );
 }
--- src/CallPact.js
+++ src/CallPact.js
@@ -1,11 +1,5 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import FilledInput from '@material-ui/core/FilledInput';
-import InputLabel from '@material-ui/core/InputLabel';
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import { VpnKey, AccountBox, ArrowLeft } from '@material-ui/icons';
-import Select from '@material-ui/core/Select';
+import { Button, Grid, Dropdown, Input, Icon, Modal, Header } from 'semantic-ui-react';
 
 import Pact from "pact-lang-api";
 import  './main.css';
@@ -14,6 +8,8 @@ const hosts = ["us1","us2","eu1","eu2","ap1","ap2"]
 const chainIds = ["0","1",'2',"3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19"]
 const createAPIHost = (network, chainId) => `https://${network}.testnet.chainweb.com/chainweb/0.0/testnet02/chain/${chainId}/pact`
 const dumKeyPair = Pact.crypto.genKeyPair();
+
+
 
 class CallPact extends React.Component {
 
@@ -24,14 +20,23 @@ class CallPact extends React.Component {
     host: 0,
     haveAccount: undefined,
     status: "notStarted",
-    reqKey: ""
+    reqKey: "",
+    modalOpen: false,
+    modalMsg: "",
+    modalHeader: "",
+    modalError: ""
   }
 
   onChangeAccountName = e => this.setState({accountName: e.target.value})
 
   onChangePublicKey = e => this.setState({publicKey: e.target.value})
 
-  onChangeChainId = e => this.setState({chainId: e.target.value})
+  onChangeChainId = (e, v) => this.setState({chainId: v.value})
+
+  handleOpen = () => this.setState({ modalOpen: true })
+
+  handleClose = () => this.setState({ modalOpen: false, modalError: "" })
+
 
   changeStatus = status => {
     this.setState({
@@ -50,14 +55,18 @@ class CallPact extends React.Component {
       pactCode: `(coin.account-balance ${JSON.stringify(this.state.accountName)})`,
       keyPairs: dumKeyPair,
     }, createAPIHost(hosts[this.state.host], this.state.chainId))
-    if (accountCheck.status==="success") alert(`${this.state.accountName} ALREADY EXISTS ON CHAIN ${this.state.chainId}`)
+    if (accountCheck.status==="success") {
+      this.setState({ modalMsg: `Account ${this.state.accountName} already exists on chain ${this.state.chainId}`, modalHeader: 'EXISTING ACCOUNT'})
+      this.handleOpen();
+      // alert(`${this.state.accountName} ALREADY EXISTS ON CHAIN ${this.state.chainId}`)
+    }
     else {
       this.setState({status: "started"})
       const reqKey = await Pact.fetch.send({
         pactCode:`(heekyun-faucet1.create-and-request-coin ${JSON.stringify(this.state.accountName)} (read-keyset 'fund-keyset) 10.0)`,
         keyPairs: dumKeyPair,
         envData: {"fund-keyset": [this.state.publicKey]},
-        meta: Pact.lang.mkMeta("heekyun-faucet1",this.state.chainId,10,50)}, createAPIHost(hosts[this.state.host], this.state.chainId))
+        meta: Pact.lang.mkMeta("heekyun-faucet1",this.state.chainId,0.0000001,50)}, createAPIHost(hosts[this.state.host], this.state.chainId))
       if (reqKey) {
         this.setState({status: "waiting"})
         this.setState({reqKey: reqKey.requestKeys[0]})
@@ -70,16 +79,24 @@ class CallPact extends React.Component {
         pactCode: `(coin.account-balance ${JSON.stringify(this.state.accountName)})`,
         keyPairs: dumKeyPair,
       }, createAPIHost(hosts[this.state.host], this.state.chainId))
-      if (accountCheck.status==="failure") alert(`ACCOUNT DOES NOT EXIST ON CHAIN ${this.state.chainId}`)
+      if (accountCheck.status==="failure") {
+        // alert(`ACCOUNT DOES NOT EXIST ON CHAIN ${this.state.chainId}`)
+        // this.createAlert('No Account', `ACCOUNT DOES NOT EXIST ON CHAIN ${this.state.chainId}`)
+        // this.handleOpen();
+        this.setState({ modalMsg: `Account ${this.state.accountName} does not exist on chain ${this.state.chainId}`, modalHeader: 'NO ACCOUNT'})
+        this.handleOpen();
+      }
       else {
         this.setState({status: "started"})
+        console.log(`(heekyun-faucet1.request-coin ${JSON.stringify(this.state.accountName)} 10.0)`)
         const reqKey = await Pact.fetch.send({
           pactCode:`(heekyun-faucet1.request-coin ${JSON.stringify(this.state.accountName)} 10.0)`,
           keyPairs: dumKeyPair,
-          meta: Pact.lang.mkMeta("heekyun-faucet1",this.state.chainId, 10,50)
+          meta: Pact.lang.mkMeta("heekyun-faucet1",this.state.chainId, 0.0000001,50)
         }, createAPIHost(hosts[this.state.host], this.state.chainId))
 
         if (reqKey) {
+          console.log(reqKey);
           this.setState({status: "waiting"})
           this.setState({reqKey: reqKey.requestKeys[0]})
         }
@@ -91,13 +108,20 @@ class CallPact extends React.Component {
     Pact.fetch.poll({requestKeys: [reqKey]}, createAPIHost(hosts[this.state.host], this.state.chainId))
     .then(res => {
       if (!res[0]) {
-        alert("We're preparing the coin...")
+        this.setState({ modalMsg: "We're preparing the coin...", modalHeader: 'TX PENDING'})
+        this.handleOpen();
+        // alert("We're preparing the coin...")
       }
       else if (res[0].result.status==="success") {
-        alert(`10 coins are funded to ${this.state.accountName}!`)
+        this.setState({ modalMsg: `10 coins are funded to ${this.state.accountName}!`, modalHeader: 'TX SUCCESS'})
+        this.handleOpen();
+        // alert(`10 coins are funded to ${this.state.accountName}!`)
         this.setState({status: ""})}
       else if (res[0].result.status==="failure") {
-        alert("Sorry, we couln't fund your account")
+        console.log(res);
+        this.setState({ modalMsg: `Sorry, we couln't fund your account`, modalHeader: 'TX FAILURE', modalError: `ERROR: ${res[0].result.error.message}` })
+        this.handleOpen();
+        // alert("Sorry, we couln't fund your account")
         this.setState({status: ""})
       }
     })
@@ -109,79 +133,138 @@ class CallPact extends React.Component {
       keyPairs: dumKeyPair,
     }, createAPIHost(hosts[this.state.host], this.state.chainId))
     .then(res => {
-      if (res.status==="failure") alert(`ACCOUNT DOES NOT EXIST ON CHAIN ${this.state.chainId}`)
-      else alert (`${this.state.accountName} has ${res.data} Faucet Coins On  chain ${this.state.chainId}`)
+      if (res.status==="failure") {
+        this.setState({ modalMsg: `Account ${this.state.accountName} does not exist on chain ${this.state.chainId}`, modalHeader: 'NO ACCOUNT'})
+        this.handleOpen();
+        // alert(`ACCOUNT DOES NOT EXIST ON CHAIN ${this.state.chainId}`)
+      }
+      else{
+        this.setState({ modalMsg: `${this.state.accountName} has ${res.data} Faucet Coins on chain ${this.state.chainId}`, modalHeader: 'USER EXISTS'})
+        this.handleOpen();
+        // alert (`${this.state.accountName} has ${res.data} Faucet Coins On  chain ${this.state.chainId}`)
+      }
     })
   }
 
   render() {
     return (
     <div>
-      <h3>Enter Your Account Name and Receive 10 coins!</h3>
+      <Grid textAlign='center'>
+        <Grid.Row textAlign='justified'>
+          <text
+            style={{ color: "#373A3C",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    fontFamily: 'Roboto',
+                    marginBottom: 10
+            }}
+          >
+            Welcome to the
+          </text>
+        </Grid.Row>
+        <Grid.Row textAlign='justified'>
+          <text
+            style={{ color: "#373A3C",
+                    fontSize: 50,
+                    fontWeight: "bold",
+                    fontFamily: 'Roboto',
+                    marginBottom: 10
+            }}
+          >
+            Kadena Testnet Faucet
+          </text>
+        </Grid.Row>
+        <Grid.Row textAlign='justified' style={{marginBottom: 0}}>
+          <text
+            style={{ color: "#373A3C",
+                    fontSize: 26,
+                    fontFamily: 'Roboto'
+            }}
+          >
+            Enter Your Account Name and Receive 10 coins!
+          </text>
+        </Grid.Row>
       {this.state.haveAccount===undefined
        ?
         <div  className = "login-container">
-          <Button variant="contained"
-          className="custom-button"
-          variant="contained"
-          style={{ marginBottom: 10, marginTop: 10 }}
-          onClick={() => this.changeStatus(true)}
+          <Button
+            className="custom-button"
+            // color={"#B54FA3"}
+            variant="contained"
+            style={{ marginBottom: 20, marginTop: 20 }}
+            onClick={() => this.changeStatus(true)}
           >
-          Have an Account?
+            <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "white"}}>Have an Account?</text>
           </Button>
 
-          <Button variant="contained"
+          <Button
+            // color={"#B54FA3"}
             className="custom-button"
             variant="contained"
-            style={{ marginBottom: 10, marginTop: 10 }}
+            style={{ marginTop: 20 }}
             onClick={() => this.changeStatus(false)}
           >
-          Don't Have an Account?
+           <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "white"}}>Don't Have an Account?</text>
           </Button>
           </div>
        :
         <div className = "login-container">
           <div>
-            <a onClick = {() => this.changeStatus(undefined)}>
-              <ArrowLeft style={{ fontSize: 50 }}/>
-            </a>
-            <FormControl
-              variant="filled"
-              style={{ minWidth: 200,
-                       maxWidth: 300}}>
-              <InputLabel htmlFor="select-multiple">CHAIN ID</InputLabel>
-              <Select
-                onChange={this.onChangeChainId}
-                value={this.state.chainId}
-                input={<FilledInput name="ChainID" id="chainId" />}
+            <Grid.Row>
+              <Grid columns={3}>
+                <Grid.Column>
+                    <a onClick = {() => this.changeStatus(undefined)}>
+                      <Icon name='arrow left' style={{ color: "#B54FA3" }}/>
+                      <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "#B54FA3"}}>back</text>
+                    </a>
+                </Grid.Column>
+              </Grid>
+            </Grid.Row>
+            <Grid.Row style={{marginBottom: 5}}>
+              <text
+                style={{ fontSize: 14, fontWeight: "bold", fontFamily: 'Roboto', color: "black"}}
               >
-              {chainIds.map(id => <MenuItem key={id} value={id}>{id}</MenuItem>)}
-              </Select>
-            </FormControl>
+                Chain ID
+              </text>
+            </Grid.Row>
+            <Grid.Row style={{ marginBottom: 0 }}>
+              <Dropdown
+                placeholder={'Select Chain ID'}
+                search
+                selection
+                onChange={this.onChangeChainId}
+                options={chainIds.map(id => ({ key: id, text: id, value: id }))}
+              >
+              </Dropdown>
+            </Grid.Row>
           {this.state.haveAccount===true
           ?
           <div>
-            <div className="login-input-container">
-              <div>
-                  <AccountBox/>
-              </div>
-              <input
-                onChange={this.onChangeAccountName}
-                value={this.state.accountName}
-                placeholder="Account Name"
-              />
-            </div>
-              <Button variant="contained"
+
+
+              <Grid.Row style={{marginBottom: 5}}>
+                <text
+                  style={{ fontSize: 14, fontWeight: "bold", fontFamily: 'Roboto', color: "black"}}
+                >
+                  Account Name
+                </text>
+              </Grid.Row>
+              <Grid textAlign='center'>
+                <Grid.Row>
+                  <Input icon='user' iconPosition='left' placeholder='Account Name' onChange={this.onChangeAccountName} />
+                </Grid.Row>
+              </Grid>
+
+
+              <Button
                 disabled={this.state.chainId==="" || this.state.status !== "notStarted"}
-                color="primary"
                 className="custom-button"
-                variant="contained"
-                style={{ marginBottom: 10, marginTop: 10 }}
+                style={{ marginBottom: 10, marginTop: 30 }}
                 onClick={() => {
                   this.fundAccount();
                 }}
               >
-                Fund Account
+                <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "white"}}>Fund Account</text>
               </Button>
               <Button variant="contained"
                 disabled={this.state.chainId===""}
@@ -192,41 +275,41 @@ class CallPact extends React.Component {
                   this.accountBalance();
                 }}
               >
-              Check Your Balance
+                <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "white"}}>Check Your Balance</text>
               </Button>
             </div>
           :
             <div>
-              <div className="login-input-container">
-                <div>
-                    <AccountBox/>
-                </div>
-                <input
-                  onChange={this.onChangeAccountName}
-                  value={this.state.accountName}
-                  placeholder="Account Name"
-                />
-              </div>
-              <div className="login-input-container">
-                <div>
-                    <VpnKey/>
-                </div>
-                <input
-                  onChange={this.onChangePublicKey}
-                  value={this.state.publicKey}
-                  placeholder="Public Key"
-                />
-              </div>
+                <Grid.Row style={{marginBottom: 5, marginTop: 10}}>
+                  <text
+                    style={{ fontSize: 14, fontWeight: "bold", fontFamily: 'Roboto', color: "black"}}
+                  >
+                    Account Name
+                  </text>
+                </Grid.Row>
+                <Grid.Row>
+                  <Input icon='user' iconPosition='left' placeholder='Account Name' onChange={this.onChangeAccountName} />
+                </Grid.Row>
+                <Grid.Row style={{marginBottom: 5, marginTop: 10}}>
+                  <text
+                    style={{ fontSize: 14, fontWeight: "bold", fontFamily: 'Roboto', color: "black"}}
+                  >
+                    Public Key
+                  </text>
+                </Grid.Row>
+                <Grid.Row>
+                  <Input icon='key' iconPosition='left' placeholder='Public Key' onChange={this.onChangePublicKey} />
+                </Grid.Row>
               <Button
                 variant="contained"
                 disabled={this.state.chainId==="" || this.state.publicKey.length !== 64 || this.state.status !== "notStarted"}
                 className="custom-button"
-                style={{ marginBottom: 10, marginTop: 10 }}
+                style={{ marginBottom: 10, marginTop: 30 }}
                 onClick={() => {
                   this.fundCreateAccount();
                 }}
               >
-                Create and Fund Account
+                <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "white"}}>Create and Fund Account</text>
               </Button>
 
           </div>
@@ -235,18 +318,53 @@ class CallPact extends React.Component {
            ? ""
            : this.state.status==="started"
              ? <p>Waiting for your request key</p>
-             : <div>
-                 <p style={{fontSize: 20}}>Your Request Key is:</p>
-                 <p style={{fontSize: 15}}>{this.state.reqKey}</p>
+             : <div style={{ marginTop: 10}}>
+
+                  <Grid.Row>
+                     <text style={{fontSize: 20, fontFamily: 'Roboto', color: 'black'}}>Your Request Key is:</text>
+                  </Grid.Row>
+                  <Grid.Row style={{ marginBottom: 20 }}>
+                     <text style={{fontSize: 15, fontFamily: 'Roboto', color: 'black'}}>{this.state.reqKey}</text>
+                  </Grid.Row>
+
                  <Button
                    className="status-button"
                    onClick={() => this.checkSuccess(this.state.reqKey)}>
-                 Check Request Status
+                  <text style={{ fontSize: 20, fontWeight: "bold", fontFamily: 'Roboto', color: "white"}}>Check Request Status</text>
                  </Button>
                </div>
            }
         </div>
         </div>
+      }
+      </Grid>
+      {this.state.modalOpen === false
+      ? <div></div>
+      :
+      <div>
+      <Modal
+        // trigger={<Button onClick={this.handleOpen}>Show Modal</Button>}
+        open={this.state.modalOpen}
+        onClose={this.handleClose}
+        basic
+        size='small'
+      >
+        <Header icon="exchange" content={this.state.modalHeader} />
+        <Modal.Content>
+          <h3>{this.state.modalMsg}</h3>
+          <h3>{this.state.modalError}</h3>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button
+            color="green"
+            onClick={this.handleClose}
+            inverted
+          >
+            <Icon name="checkmark" /> Got it
+          </Button>
+        </Modal.Actions>
+      </Modal>
+      </div>
       }
     </div>
   )
